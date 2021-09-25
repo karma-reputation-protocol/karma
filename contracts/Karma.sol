186d13c7088377f446f0d2a5e304c21aed520c6a
Javier Roberts <javierroberts@Javiers-MacBook-Pro.local>
2021-09-25 15:20:15 -0400

    Modified existing arithmetic to prevent overflow

--- contracts/Karma.sol
+++ contracts/Karma.sol
@@ -1,26 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import {SafeMath} from '../dependencies/SafeMath.sol';
+
 contract Karma {
+  using SafeMath for uint256;
+
   address admin;
-  mapping(address => mapping(address => uint)) public karmaMap;
+  mapping(address => mapping(address => uint256)) public karmaMap;
   mapping(address => bool) public isAuthorized;
+
   constructor()  {
     admin = msg.sender;
   }
-
-  function getKarma(address appAddr, address addr) public view returns (uint karma) {
+  function getKarma(address appAddr, address addr) public view returns (uint256 karma) {
       karma = karmaMap[appAddr][addr];
   }
-  function raiseKarma(address addr, uint amount) public payable {
+  function raiseKarma(address addr, uint256 amount) public payable {
       require(msg.value > 1);
       require(isAuthorized[msg.sender], "Sender not authorized.");
-      karmaMap[msg.sender][addr] += amount;
+      karmaMap[msg.sender][addr] = karmaMap[msg.sender][addr].add(amount);
   }
-  function lowerKarma(address addr, uint amount) public payable {
+  function lowerKarma(address addr, uint256 amount) public payable {
       require(msg.value > 1);
       require(isAuthorized[msg.sender]);
-      karmaMap[msg.sender][addr] -= amount;
+      karmaMap[msg.sender][addr] = karmaMap[msg.sender][addr].sub(amount);
   }
   function authorize(address addr) public {
       require(msg.sender == admin);
